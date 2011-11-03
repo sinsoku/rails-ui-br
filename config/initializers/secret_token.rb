dbe6969a8fa8a49abe2f10527fb99afeddb7b414
sinsoku <sinsoku.listy@gmail.com>
2011-11-04 01:41:26 +0900

    rails new ../rails-ui-br --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsUiBr::Application.config.secret_token = '54a571c3e7c120caf673c379f3e1a5a0908f3b162c6de7ce150e377230176a8134dbf043bd3ce92f9b2cf7556a0fad004b3552596bfbd339a69e66b266e0adb9'
